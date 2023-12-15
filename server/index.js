45c8a90904afaac5b6f15bcff97187a111e33582
Verified ✔️ <ryesan58@gmail.com>
2023-12-15 18:08:42 -0500

    finished

--- server/index.js
+++ server/index.js
@@ -1,20 +1,17 @@
-const express = require('express');
+const express = require('express')
 
-const path = require('path');
-
+const path = require('path')
 const app = express();
 
-const PORT = 3000;
-
-const clientPath = path.resolve(__dirname, '../client/dist');
-
-app.use(express.static(clientPath));
+const clientPath = path.resolve(__dirname, '../client/dist')
+app.use(express.static(clientPath))
 
 app.get('/', (req, res) => {
-    res.sendFile(path.resolve(__dirname, '../client/dist', 'index.html'));
-});
+  // we'll do some stuff here
+  res.sendFile(path.resolve(__dirname, '../client/dist', 'index.html'))
+})
 
+const PORT = 3000;
 app.listen(PORT, () => {
-    console.log(`Server listening on port ${PORT}`);
-});
-
+  console.log(`Server listening on port ${PORT}`)
+})
